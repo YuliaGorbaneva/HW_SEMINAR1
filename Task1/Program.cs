640648c1270911f99dd5c35fd2b40858ef361c39
Yulia <yarantseva89@bk.ru>
2023-04-10 22:38:13 +0600

    max_two_numbers

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает два числа ивыдает, какое число больше
+
+Console.Write("Видите чило: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b){
+    System.Console.WriteLine($"max = {a}");
+}
+else {
+    System.Console.WriteLine($"max = {b}"); 
+}
